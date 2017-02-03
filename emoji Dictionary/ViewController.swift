91df3fdee8edfc7f4453a2b0829301f7df5fc4df
Dhruv Mishra <you@example.com>
2017-02-03 14:31:38 +0530

    second commit

--- emoji Dictionary/ViewController.swift
+++ emoji Dictionary/ViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var mytableview: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mytableview.dataSource = self
+        mytableview.delegate = self
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+        
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell ()
+        cell.textLabel?.text = "hello"
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
