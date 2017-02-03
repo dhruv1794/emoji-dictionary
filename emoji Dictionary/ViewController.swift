8497869fc7b039e085a59a037062c3cf377e5cb9
Dhruv Mishra <you@example.com>
2017-02-03 16:13:13 +0530

    my emoji page

--- emoji Dictionary/ViewController.swift
+++ emoji Dictionary/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var mytableview: UITableView!
+    
+    var emojis = ["😀","🤗","😕" ,"😂" ,"👠" ,"💩","😨" ]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,12 +22,12 @@ class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegat
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return emojis.count
         
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell ()
-        cell.textLabel?.text = "  😂 "
+        cell.textLabel?.text =  emojis[indexPath.row]
         return cell
     }
 
